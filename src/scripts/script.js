c14b12e774aaf3143c7fa394b8e8469000782d3c
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2024-06-11 16:06:27 +0300

    add burger menu

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,24 @@
-console.log('dgsdgs')
-console.log('ffff')
-console.log('Good')
+// Elements
+const openBurgerMenuBtnElement = document.getElementById('openBurgerMenuBtn');
+const closeBurgerMenuBtnElement = document.getElementById('closeBurgerMenuBtn');
+
+// Listeners
+openBurgerMenuBtnElement.addEventListener('click', handleClickOpenMenu);
+closeBurgerMenuBtnElement.addEventListener('click', handleClickCloseMenu);
+
+// Functions
+function handleClickOpenMenu() {
+  toggleClassess();
+}
+
+function handleClickCloseMenu() {
+  toggleClassess();
+}
+
+// Helpers
+function toggleClassess() {
+  const burgerMenuElement = document.getElementById('burgerMenu');
+  const bodyElement = document.getElementsByTagName('body')[0];
+  burgerMenuElement.classList.toggle('burger__menu_display-block');
+  bodyElement.classList.toggle('hidden');
+}
